c9753d8ae403899aaf5d5872e0692f737221a603
Susan Choi <2539lee@gmail.com>
2022-04-27 07:56:28 -0700

    Current Location

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import logo from './logo.svg'
 import './App.css'
+import { useEffect, useState } from 'react'
 
+// 1. When app is started, it shows weather based on location
+// 2. City, C/F degrees, weather information
+// 3. 5 buttons: 1(current city) 4(other cities)
+// 4. Button clicked, it shows the weather of the city
+// 5. Current button clicked, it shows weather of the current one.
+// 6. Loading spinner spins when data is loading
 function App() {
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude
+      let lon = position.coords.longitude
+      console.log('current location:', lat, lon)
+    })
+  }
+
+  useEffect(() => {
+    getCurrentLocation()
+  }, [])
+
   return <div>Test~~</div>
 }
 
